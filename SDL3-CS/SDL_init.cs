c1de0c9726bf5a45f531f0ce4701b83a63174421
eduardgushchin <eduardgushchin@yandex.ru>
2024-07-06 18:35:58 +0300

    Update init documentation

--- SDL3-CS/SDL_init.cs
+++ SDL3-CS/SDL_init.cs
@@ -60,7 +60,7 @@ public static partial class SDL
         /// Initializes the joystick subsystem.
         /// <see cref="Joystick"/> implies <see cref="Events"/>
         /// Should be initialized on the same thread as <see cref="Video"/>
-        /// on Windows if you don't set SDL_HINT_JOYSTICK_THREAD
+        /// on Windows if you don't set <see cref="SDL.HintJoystickThread"/>
         /// </summary>
         Joystick = 0x00000200u,
         
@@ -106,15 +106,34 @@ public static partial class SDL
     /// call <see cref="GetError"/> for more information.
     /// </returns>
     /// <remarks>
-    /// <see cref="Init"/> simply forwards to calling <see cref="InitSubSystem"/>.
-    /// Therefore, the two may be used interchangeably.
-    /// Though for readability of your code <see cref="InitSubSystem"/> might be preferred.
-    /// The file I/O (for example: <see cref="IOFromFile"/>) and threading (<see cref="CreateThread"/>)
-    /// subsystems are initialized by default.
-    /// Message boxes (<see cref="ShowSimpleMessageBox"/>) also attempt to work without initializing the
-    /// video subsystem, in hopes of being useful in showing an error dialog when <see cref="Init"/> fails.
-    /// You must specifically initialize other subsystems if you use them in your application.
+    /// <para><see cref="Init"/> simply forwards to calling <see cref="InitSubSystem"/>.
+    /// Therefore, the two may be used</para>
+    /// <para>The file I/O (for example: <see cref="IOFromFile"/>) and threading (<see cref="CreateThread"/>)
+    /// subsystems are initialized by default. Message boxes (<see cref="ShowSimpleMessageBox"/>) also attempt to work
+    /// without initializing the video subsystem, in hopes of being useful in showing an error dialog when
+    /// <see cref="Init"/> fails. You must specifically initialize other subsystems if you use them in your
+    /// application.</para>
+    /// <para>Logging (such as <see cref="Log"/>) works without initialization, too.</para>
+    /// <para>flags may be any of the following OR'd together:</para>
+    /// <list type="bullet">
+    /// <item><see cref="InitFlags.Timer"/>: timer subsystem</item>
+    /// <item><see cref="InitFlags.Audio"/>: audio subsystem; automatically initializes the events subsystem</item>
+    /// <item><see cref="InitFlags.Video"/>: video subsystem; automatically initializes the events subsystem</item>
+    /// <item><see cref="InitFlags.Joystick"/>: joystick subsystem; automatically initializes the events subsystem</item>
+    /// <item><see cref="InitFlags.Haptic"/>: haptic (force feedback) subsystem</item>
+    /// <item><see cref="InitFlags.Gamepad"/>: gamepad subsystem; automatically initializes the joystick subsystem</item>
+    /// <item><see cref="InitFlags.Events"/>: events subsystem</item>
+    /// <item><see cref="InitFlags.Sensor"/>: sensor subsystem; automatically initializes the events subsystem</item>
+    /// <item><see cref="InitFlags.Camera"/>: camera subsystem; automatically initializes the events subsystem</item>
+    /// </list>
+    /// <para>Subsystem initialization is ref-counted, you must call <see cref="QuitSubSystem"/> for each
+    /// <see cref="InitSubSystem"/> to correctly shutdown a subsystem manually (or call <see cref="Quit"/> to
+    /// force shutdown). If a subsystem is already loaded then this call will increase the ref-count and return.</para>
     /// </remarks>
+    /// <seealso cref="InitSubSystem"/>
+    /// <seealso cref="Quit"/>
+    /// <seealso cref="SetMainReady"/>
+    /// <seealso cref="WasInit"/>
     public static int Init(InitFlags flags) => SDL_Init(flags);
 
     
@@ -133,8 +152,11 @@ public static partial class SDL
     /// call <see cref="GetError"/> for more information.
     /// </returns>
     /// <remarks>
-    /// This function and SDL_Init() are interchangeable.
+    /// This function and <see cref="Init"/> are interchangeable.
     /// </remarks>
+    /// <seealso cref="Init"/>
+    /// <seealso cref="Quit"/>
+    /// <seealso cref="QuitSubSystem"/>
     public static int InitSubSystem(InitFlags flags) => SDL_InitSubSystem(flags);
     
     
@@ -165,6 +187,8 @@ public static partial class SDL
     /// Returns a mask of all initialized subsystems if flags is 0,
     /// otherwise it returns the initialization status of the specified subsystems.
     /// </returns>
+    /// <seealso cref="Init"/>
+    /// <seealso cref="InitSubSystem"/>
     public static InitFlags WasInit(InitFlags flags) => SDL_WasInit(flags);
     
     
@@ -175,10 +199,13 @@ public static partial class SDL
     /// Clean up all initialized subsystems.
     /// </summary>
     /// <remarks>
-    /// You should call this function even if you have already shutdown each initialized subsystem with
-    /// <see cref="QuitSubSystem"/>. It is safe to call this function even in the case of errors in initialization.
-    /// You can use this function with atexit() to ensure that it is run when your application is shutdown,
-    /// but it is not wise to do this from a library or other dynamically loaded code.
+    /// <para>You should call this function even if you have already shutdown each initialized subsystem with
+    /// <see cref="QuitSubSystem"/>. It is safe to call this function even in the case of errors in
+    /// initialization.</para>
+    /// <para>You can use this function with atexit() to ensure that it is run when your application is shutdown,
+    /// but it is not wise to do this from a library or other dynamically loaded code.</para>
     /// </remarks>
+    /// <seealso cref="Init"/>
+    /// <seealso cref="QuitSubSystem"/>
     public static void Quit() => SDL_Quit();
 }
