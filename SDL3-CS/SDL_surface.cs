727140673a61db20cdbfe14501cd50a027a98b10
eduardgushchin <eduardgushchin@yandex.ru>
2024-07-12 18:50:27 +0300

    Started porting SDL_surface

---
+++ SDL3-CS/SDL_surface.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SDL3;
+
+public static partial class SDL
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Surface
+    {
+        //SDL_SurfaceFlags flags;     /**< Read-only */
+        //SDL_PixelFormat format;     /**< Read-only */
+        //int w, h;                   /**< Read-only */
+        //int pitch;                  /**< Read-only */
+        //void *pixels;               /**< Read-only pointer, writable pixels if non-NULL */
+
+        //int refcount;               /**< Application reference count, used when freeing surface */
+
+        //SDL_SurfaceData *internal;  /**< Private */
+
+    }
+}
