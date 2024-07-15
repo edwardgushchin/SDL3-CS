d262d95f8ce05587a82852bb0f1bf3a1a33eaab0
eduardgushchin <eduardgushchin@yandex.ru>
2024-07-15 21:44:49 +0300

    Add SensorType

---
+++ SDL3-CS/SDL/SDL_sensor.cs
@@ -0,0 +1,16 @@
+﻿namespace SDL3;
+
+public static partial class SDL
+{
+    public enum SensorType
+    {
+        Invalid = -1,    /**< Returned for an invalid sensor */
+        Unknown,         /**< Unknown sensor type */
+        Accel,           /**< Accelerometer */
+        Gyro,            /**< Gyroscope */
+        AccelL,         /**< Accelerometer for left Joy-Con controller and Wii nunchuk */
+        GyroL,          /**< Gyroscope for left Joy-Con controller */
+        AccelR,         /**< Accelerometer for right Joy-Con controller */
+        GyroR           /**< Gyroscope for right Joy-Con controller */
+    }
+}
