bdc265f3739b112f4e3978618b09ebbc8414947e
Eduard Gushchin <eduardgushchin@yandex.ru>
2024-07-18 00:10:14 +0300

    Added GetGamepadMappings method

--- SDL3-CS/SDL/gamepad/PInvoke.cs
+++ SDL3-CS/SDL/gamepad/PInvoke.cs
@@ -49,4 +49,30 @@ public static partial class SDL
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     private static partial int SDL_ReloadGamepadMappings();
     public static int ReloadGamepadMappings() => SDL_ReloadGamepadMappings();
+    
+    
+    [LibraryImport(SDLLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static unsafe partial IntPtr SDL_GetGamepadMappings(out int count);
+    public static string?[] GetGamepadMappings(out int count)
+    {
+        var arrayPtr = SDL_GetGamepadMappings(out count);
+        
+        if (arrayPtr == IntPtr.Zero || count == 0)
+        {
+            return [];
+        }
+
+        var mappings = new string?[count];
+        var ptrArray = new IntPtr[count];
+        Marshal.Copy(arrayPtr, ptrArray, 0, count);
+        Free(arrayPtr);
+
+        for (var i = 0; i < count; i++)
+        {
+            mappings[i] = Marshal.PtrToStringAnsi(ptrArray[i]);
+        }
+
+        return mappings;
+    }
 }
