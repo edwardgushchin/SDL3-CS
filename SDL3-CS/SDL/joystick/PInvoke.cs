4604e5c8b3cfe2b303339c99aba3282deb4483c8
Eduard Gushchin <eduardgushchin@yandex.ru>
2024-07-17 10:13:54 +0300

    Split SDL_joystick.cs

--- SDL3-CS/SDL/joystick/PInvoke.cs
+++ SDL3-CS/SDL/joystick/PInvoke.cs
@@ -33,57 +33,6 @@ namespace SDL3;
 
 public static partial class SDL
 {
-	
-	public struct Joystick(IntPtr handle)
-	{
-		public IntPtr Handle { get; } = handle;
-
-		public override bool Equals(object? obj)
-		{
-			return obj is Cursor other && Handle == other.Handle;
-		}
-
-		public override int GetHashCode()
-		{
-			return Handle.GetHashCode();
-		}
-        
-		public static bool operator ==(Joystick left, Joystick right)
-		{
-			return left.Equals(right);
-		}
-
-		public static bool operator !=(Joystick left, Joystick right)
-		{
-			return !(left == right);
-		}
-	}
-
-	
-	public enum JoystickType
-	{
-		Unknown,
-		Gamepad,
-		Wheel,
-		ArcadeStick,
-		FlightStick,
-		DancePad,
-		Guitar,
-		DrumKit,
-		ArcadePad,
-		Throttle
-	}
-	
-	
-	public enum JoystickConnectionState
-	{
-		Invalid = -1,
-		Unknown,
-		Wired,
-		Wireless
-	}
-	
-	
 	[LibraryImport(SDLLibrary)]
 	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
 	private static partial void SDL_LockJoysticks();
@@ -142,7 +91,6 @@ public static partial class SDL
 	public static GUID GetJoystickInstanceGUID(uint instanceId) => SDL_GetJoystickInstanceGUID(instanceId);
 	
 	
-
 	[LibraryImport(SDLLibrary)]
 	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
 	private static partial ushort SDL_GetJoystickInstanceVendor(uint instanceId);
@@ -187,73 +135,6 @@ public static partial class SDL
 	private static partial IntPtr SDL_GetJoystickFromPlayerIndex(int playerIndex);
 	public static Joystick GetJoystickFromPlayerIndex(int playerIndex) =>
 		new(SDL_GetJoystickFromPlayerIndex(playerIndex));
-	
-	
-	[StructLayout(LayoutKind.Sequential)]
-	public struct VirtualJoystickTouchpadDesc
-	{
-		public UInt16 NFingers;    /**< the number of simultaneous fingers on this touchpad */
-		private unsafe fixed UInt16 Padding[3];
-	}
-	
-	[StructLayout(LayoutKind.Sequential)]
-	public struct VirtualJoystickSensorDesc
-	{
-		public SensorType Type;
-		public float Rate;
-	}
-	
-	
-	[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-	public struct VirtualJoystickDesc
-	{
-		public JoystickType Type;
-		private UInt16 padding;
-		public UInt16 VendorId;
-		public UInt16 ProductId;
-		public UInt16 NAxes;
-		public UInt16 NButtons;
-		public UInt16 NBalls;
-		public UInt16 NHats;
-		public UInt16 NTouchpads;
-		public UInt16 NSensors;
-		private unsafe fixed UInt16 padding2[2];
-		public UInt32 ButtonMask;
-		public UInt32 AxisMask;
-		public string? Name;
-		public VirtualJoystickTouchpadDesc[]? Touchpads;
-		public VirtualJoystickSensorDesc[]? Sensors;
-		public IntPtr? UserData;
-		public VirtualJoystickUpdateCallback Update;
-		public VirtualJoysticSetPlayerIndexCallback? SetPlayerIndex;
-		public VirtualJoysticRumbleCallback? Rumble;
-		public VirtualJoysticRumbleTriggersCallback? RumbleTriggers;
-		public VirtualJoysticSetLEDCallback? SetLED;
-		public VirtualJoysticSendEffectCallback? SendEffect;
-		public VirtualJoysticSetSensorsEnabledCallback? SetSensorsEnabled;
-	}
-	
-	
-	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-	public delegate void VirtualJoystickUpdateCallback(IntPtr userData);
-	
-	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-	public delegate void VirtualJoysticSetPlayerIndexCallback(IntPtr userData, int player_index);
-
-	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-	public delegate int VirtualJoysticRumbleCallback(IntPtr userData, ushort low_frequency_rumble, ushort high_frequency_rumble);
-
-	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-	public delegate int VirtualJoysticRumbleTriggersCallback(IntPtr userData, ushort left_rumble, ushort right_rumble);
-
-	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-	public delegate int VirtualJoysticSetLEDCallback(IntPtr userData, byte red, byte green, byte blue);
-
-	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-	public delegate int VirtualJoysticSendEffectCallback(IntPtr userData, IntPtr data, int size);
-
-	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-	public delegate int VirtualJoysticSetSensorsEnabledCallback(IntPtr userData, bool enabled);
 
 
 	[DllImport(SDLLibrary, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
@@ -279,27 +160,4 @@ public static partial class SDL
 	private static partial int SDL_SetJoystickVirtualAxis(IntPtr joystick, int axis, short value);
 	public static int SetJoystickVirtualAxis(Joystick joystick, int axis, short value) => 
 		SDL_SetJoystickVirtualAxis(joystick.Handle, axis, value);
-
-	
-	public const short JoystickAxisMax = short.MaxValue;
-	
-	
-	public const short JoystickAxisMin = short.MinValue;
-	
-	
-	public const float IPhoneMaxGForce = 5.0f;
-	
-	
-	public enum JoystickHat : byte
-	{
-		Centered = 0x00,
-		Up = 0x01,
-		Right = 0x02,
-		Down = 0x04,
-		Left = 0x08,
-		RightUp = Right | Up,
-		RightDown = Right | Down,
-		LeftUp = Left | Up, 
-		LeftDown = Left | Down
-	}
 }
---
+++ SDL3-CS/SDL/joystick/VirtualJoystickDesc.cs
@@ -0,0 +1,63 @@
+﻿#region License
+/* SDL3# - C# Wrapper for SDL3
+ *
+ * Copyright (c) 2024 Eduard Gushchin.
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from
+ * the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software in a
+ * product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ * Eduard "edwardgushchin" Gushchin <eduardgushchin@yandex.ru>
+ *
+ */
+#endregion
+
+using System.Runtime.InteropServices;
+
+namespace SDL3;
+
+public static partial class SDL
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct VirtualJoystickDesc
+    {
+        public JoystickType Type;
+        private UInt16 padding;
+        public UInt16 VendorId;
+        public UInt16 ProductId;
+        public UInt16 NAxes;
+        public UInt16 NButtons;
+        public UInt16 NBalls;
+        public UInt16 NHats;
+        public UInt16 NTouchpads;
+        public UInt16 NSensors;
+        private unsafe fixed UInt16 padding2[2];
+        public UInt32 ButtonMask;
+        public UInt32 AxisMask;
+        public string? Name;
+        public VirtualJoystickTouchpadDesc[]? Touchpads;
+        public VirtualJoystickSensorDesc[]? Sensors;
+        public IntPtr? UserData;
+        public VirtualJoystickUpdateCallback Update;
+        public VirtualJoysticSetPlayerIndexCallback? SetPlayerIndex;
+        public VirtualJoysticRumbleCallback? Rumble;
+        public VirtualJoysticRumbleTriggersCallback? RumbleTriggers;
+        public VirtualJoysticSetLEDCallback? SetLED;
+        public VirtualJoysticSendEffectCallback? SendEffect;
+        public VirtualJoysticSetSensorsEnabledCallback? SetSensorsEnabled;
+    }
+}
