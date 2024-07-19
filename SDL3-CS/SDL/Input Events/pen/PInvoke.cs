22ddcd0fadc31fa8320ba90efbe56106fb021647
Eduard Gushchin <eduardgushchin@yandex.ru>
2024-07-19 12:01:53 +0300

    Finished porting the file SDL_pen.h

--- SDL3-CS/SDL/Input Events/pen/PInvoke.cs
+++ SDL3-CS/SDL/Input Events/pen/PInvoke.cs
@@ -26,6 +26,9 @@
  */
 #endregion
 
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
 namespace SDL3;
 
 public static partial class SDL
@@ -40,4 +43,57 @@ public static partial class SDL
     {
         return PenCapability(axis + PenFlagAxisBitOffset);
     }
+    
+    
+    [LibraryImport(SDLLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial uint SDL_GetPens(out int count);
+    public static uint GetPens(out int count) => SDL_GetPens(out count);
+    
+    
+    [LibraryImport(SDLLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial uint SDL_GetPenStatus(uint instanceID, out float x, out float y, out float axes,
+        ulong numAxes);
+    public static uint GetPenStatus(uint instanceID, out float x, out float y, out float axes, ulong numAxes) =>
+        SDL_GetPenStatus(instanceID, out x, out y, out axes, numAxes);
+    
+    
+    [LibraryImport(SDLLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial uint SDL_GetPenFromGUID(GUID guid);
+    public static uint GetPenFromGUID(GUID guid) => SDL_GetPenFromGUID(guid);
+    
+    
+    [LibraryImport(SDLLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial GUID SDL_GetPenGUID(uint instanceID);
+    public static GUID GetPenGUID(uint instanceID) => SDL_GetPenGUID(instanceID);
+    
+    
+    [LibraryImport(SDLLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    private static partial bool SDL_PenConnected(uint instanceID);
+    public static bool PenConnected(uint instanceID) => SDL_PenConnected(instanceID);
+    
+    
+    [LibraryImport(SDLLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial IntPtr SDL_GetPenName(uint instanceID);
+    public static string? GetPenName(uint instanceID) => Marshal.PtrToStringAnsi(SDL_GetPenName(instanceID));
+    
+    
+    [LibraryImport(SDLLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial PenCapabilityFlags SDL_GetPenCapabilities(uint instanceID,
+        out PenCapabilityInfo capabilities);
+    public static PenCapabilityFlags GetPenCapabilities(uint instanceID, out PenCapabilityInfo capabilities) =>
+        SDL_GetPenCapabilities(instanceID, out capabilities);
+    
+    
+    [LibraryImport(SDLLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial PenSubtype SDL_GetPenType(uint instanceID);
+    public static PenSubtype GetPenType(uint instanceID) => SDL_GetPenType(instanceID);
 }
