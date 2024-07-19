1eb6c270dc444b03a0b4315a15d445df07a55ab1
Eduard Gushchin <eduardgushchin@yandex.ru>
2024-07-19 22:33:05 +0300

    Added Sensor struct

---
+++ SDL3-CS/SDL/Input Events/sensor/Sensor.cs
@@ -0,0 +1,29 @@
+﻿namespace SDL3;
+
+public static partial class SDL
+{
+    public readonly struct Sensor(IntPtr handle)
+    {
+        public IntPtr Handle { get; } = handle;
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Sensor other && Handle == other.Handle;
+        }
+
+        public override int GetHashCode()
+        {
+            return Handle.GetHashCode();
+        }
+        
+        public static bool operator ==(Sensor left, Sensor right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Sensor left, Sensor right)
+        {
+            return !(left == right);
+        }
+    }
+}
