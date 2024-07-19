1f787892619609e4cf692e27ee1df0220aef344c
Eduard Gushchin <eduardgushchin@yandex.ru>
2024-07-19 12:00:17 +0300

    Added PenCapabilityFlags enum

---
+++ SDL3-CS/SDL/Input Events/pen/PenCapabilityFlag.cs
@@ -0,0 +1,19 @@
+﻿namespace SDL3;
+
+public static partial class SDL
+{
+    [Flags]
+	public enum PenCapabilityFlags : uint
+	{
+		Down = 1u << PenFlagDownBitIndex,
+		Ink = 1u << PenFlagInkBitIndex,
+		Eraser = 1u << PenFlagEraserBitIndex,
+		AxisPressure = 1u << (PenAxis.Pressure + PenFlagAxisBitOffset),
+		AxisXTilt = 1u << (PenAxis.XTilt + PenFlagAxisBitOffset),
+		AxisYTilt = 1u << (PenAxis.YTilt + PenFlagAxisBitOffset),
+		AxisDistance = 1u << (PenAxis.Distance + PenFlagAxisBitOffset),
+		AxisRotation = 1u << (PenAxis.Rotation + PenFlagAxisBitOffset),
+		AxisSlider = 1u << (PenAxis.Slider + PenFlagAxisBitOffset),
+		AxisBidirectional = AxisXTilt | AxisYTilt
+	}
+}
