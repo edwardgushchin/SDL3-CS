c7872e42d711b72ff901ff53a28704ae81812b66
Eduard Gushchin <eduardgushchin@yandex.ru>
2024-07-20 19:13:32 +0300

    Added GLContextResetNotification enum

---
+++ SDL3-CS/SDL/Video/video/GLContextResetNotification.cs
@@ -0,0 +1,10 @@
+﻿namespace SDL3;
+
+public static partial class SDL
+{
+    public enum GLContextResetNotification
+    {
+        NoNotification = 0x0000,
+        LoseContext    = 0x0001
+    }
+}
