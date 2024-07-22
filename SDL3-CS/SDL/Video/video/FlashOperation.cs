c0667c0315aa70a1f5675f1cedff735184cb7ee1
Eduard Gushchin <eduardgushchin@yandex.ru>
2024-07-22 20:10:21 +0300

    Added documentation from FlashOperation.cs

--- SDL3-CS/SDL/Video/video/FlashOperation.cs
+++ SDL3-CS/SDL/Video/video/FlashOperation.cs
@@ -30,10 +30,24 @@ namespace SDL3;
 
 public static partial class SDL
 {
+    /// <summary>
+    /// Window flash operation.
+    /// </summary>
     public enum FlashOperation
     {
+        /// <summary>
+        /// Cancel any window flash state
+        /// </summary>
         Cancel,
+        
+        /// <summary>
+        /// Flash the window briefly to get attention
+        /// </summary>
         Briefly,
+        
+        /// <summary>
+        /// Flash the window until it gets focus
+        /// </summary>
         UntilFocused
     }
 }
