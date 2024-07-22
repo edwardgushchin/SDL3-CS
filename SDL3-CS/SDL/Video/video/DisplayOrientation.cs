4e165a4a15fa71d775f41274a4810af51651b45b
Eduard Gushchin <eduardgushchin@yandex.ru>
2024-07-22 20:08:37 +0300

    Added documentation from DisplayOrientation.cs

--- SDL3-CS/SDL/Video/video/DisplayOrientation.cs
+++ SDL3-CS/SDL/Video/video/DisplayOrientation.cs
@@ -30,12 +30,34 @@ namespace SDL3;
 
 public static partial class SDL
 {
+    /// <summary>
+    /// Display orientation values; the way a display is rotated.
+    /// </summary>
     public enum DisplayOrientation
     {
+        /// <summary>
+        /// The display orientation can't be determined
+        /// </summary>
         Unknown,
+        
+        /// <summary>
+        /// The display is in landscape mode, with the right side up, relative to portrait mode
+        /// </summary>
         Landscape,
+        
+        /// <summary>
+        /// The display is in landscape mode, with the left side up, relative to portrait mode
+        /// </summary>
         LandscapeFlipped,
+        
+        /// <summary>
+        /// The display is in portrait mode
+        /// </summary>
         Portrait,
+        
+        /// <summary>
+        /// The display is in portrait mode, upside down
+        /// </summary>
         PortraitFlipped
     }
 }
