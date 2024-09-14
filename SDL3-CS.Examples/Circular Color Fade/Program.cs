166aa6908d41b935b970e9f3871a56160f819d30
Eduard Gushchin <eduardgushchin@yandex.ru>
2024-09-14 21:57:35 +0300

    Removed trash

--- SDL3-CS.Examples/Circular Color Fade/Program.cs
+++ SDL3-CS.Examples/Circular Color Fade/Program.cs
@@ -51,17 +51,6 @@ internal static class Program
             return;
         }
 
-        var basePath = SDL.GetBasePath();
-        
-        Console.WriteLine($"BasePath: {basePath}");
-
-        var rect = new SDL.Rect(0, 0, 100, 100);
-        var point = new SDL.Point(-10, 25);
-
-        var pointInRect = SDL.PointInRect(point, rect);
-        
-        Console.WriteLine($"PointInRect: {pointInRect}");
-
         var loop = true;
         var startCounter = SDL.GetPerformanceCounter();
         var frequency = SDL.GetPerformanceFrequency();
