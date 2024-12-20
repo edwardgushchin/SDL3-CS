d732ffbe1b2a89f802328f496d7ce41e6d45bb50
Eduard Gushchin <eduardgushchin@yandex.ru>
2024-12-20 14:31:36 +0300

    Commit: https://github.com/libsdl-org/SDL/commit/b2c15a8c2a3236825df611c01235f8cd2ba4da59

--- SDL3-CS/GPU/gpu/PInvoke.cs
+++ SDL3-CS/GPU/gpu/PInvoke.cs
@@ -305,11 +305,12 @@ public partial class SDL
     /// [[stage_in]] attribute which will automatically use the vertex input
     /// information from the SDL_GPUGraphicsPipeline.</item>
     /// </list>
-    /// <para>Shader semantics do not matter in D3D12 and for ease of use the SDL
-    /// implementation assumes that they will all be TEXCOORD. If you are using
-    /// HLSL as the shader source language, your vertex semantics should start at
-    /// TEXCOORD0 and increment like so: TEXCOORD1, TEXCOORD2, etc. If you wish to
-    /// change the semantic prefix to something other than TEXCOORD you can use
+    /// <para>Shader semantics other than system-value semantics do not matter in D3D12
+    /// and for ease of use the SDL implementation assumes that non system-value
+    /// semantics will all be TEXCOORD. If you are using HLSL as the shader source
+    /// language, your vertex semantics should start at TEXCOORD0 and increment
+    /// like so: TEXCOORD1, TEXCOORD2, etc. If you wish to change the semantic
+    /// prefix to something other than TEXCOORD you can use
     /// <see cref="Props.GPUDeviceCreateD3D12SemanticNameString"/> with
     /// <see cref="CreateGPUDeviceWithProperties"/>.</para>
     /// </summary>
