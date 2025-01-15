f83eb256a0c15992aa5798e32a3d09b047edba35
Eduard Gushchin <eduardgushchin@yandex.ru>
2025-01-15 23:54:29 +0300

    [SDL] audio: Allow streams to change the device-side channels maps. (https://github.com/edwardgushchin/SDL/commit/9e60a8994fbfd63a541848da2e30109c4f99da75)

--- SDL3-CS/SDL/Audio/audio/PInvoke.cs
+++ SDL3-CS/SDL/Audio/audio/PInvoke.cs
@@ -567,7 +567,7 @@ public static partial class SDL
     /// <para>Binding a stream to a device will set its output format for playback
     /// devices, and its input format for recording devices, so they match the
     /// device's settings. The caller is welcome to change the other end of the
-    /// stream's format at any time.</para>
+    ///stream's format at any time with <see cref="SetAudioStreamFormat"/>.</para>
     /// </summary>
     /// <param name="devid">an audio device to bind a stream to.</param>
     /// <param name="streams">an array of audio streams to bind.</param>
@@ -718,6 +718,11 @@ public static partial class SDL
     /// the end of a sound file in one format to a stream, change formats for the
     /// next sound file, and start putting that new data while the previous sound
     /// file is still queued, and everything will still play back correctly.</para>
+    /// <para>If a stream is bound to a device, then the format of the side of the stream
+    /// bound to a device cannot be changed (src_spec for recording devices,
+    /// dst_spec for playback devices). Attempts to make a change to this side
+    /// will be ignored, but this will not report an error. The other side's format
+    /// can be changed.</para>
     /// </summary>
     /// <param name="stream">the stream the format is being changed.</param>
     /// <param name="srcSpec">the new format of the audio input; if <c>null</c>, it is not
@@ -915,6 +920,10 @@ public static partial class SDL
     /// stream's format, this will fail. This is a safety measure to make sure a
     /// race condition hasn't changed the format while this call is setting the
     /// channel map.</para>
+    /// <para>Unlike attempting to change the stream's format, the input channel map on a
+    /// stream bound to a recording device is permitted to change at any time; any
+    /// data added to the stream from the device after this call will have the new
+    /// mapping, but previously-added data will still have the prior mapping.</para>
     /// </summary>
     /// <param name="stream">the SDL_AudioStream to change.</param>
     /// <param name="chmap">the new channel map, <c>null</c> to reset to default.</param>
@@ -957,6 +966,12 @@ public static partial class SDL
     /// stream's format, this will fail. This is a safety measure to make sure a
     /// race condition hasn't changed the format while this call is setting the
     /// channel map.</para>
+    /// <para>Unlike attempting to change the stream's format, the output channel map on
+    /// a stream bound to a recording device is permitted to change at any time;
+    /// any data added to the stream after this call will have the new mapping, but
+    /// previously-added data will still have the prior mapping. When the channel
+    /// map doesn't match the hardware's channel layout, SDL will convert the data
+    /// before feeding it to the device for playback.</para>
     /// </summary>
     /// <param name="stream">the SDL_AudioStream to change.</param>
     /// <param name="chmap">the new channel map, <c>null</c> to reset to default.</param>
