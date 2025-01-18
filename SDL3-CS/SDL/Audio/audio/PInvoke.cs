3e9b5af58f0aae0768edf694d4eeb58fe5dc0606
Eduard Gushchin <eduardgushchin@yandex.ru>
2025-01-18 18:22:18 +0300

    [SDL] Sync SDL3 wiki -> header (https://github.com/edwardgushchin/SDL/commit/07a5c144c651d21b41121225aa4ef2db43e74c59)

--- SDL3-CS/SDL/Audio/audio/PInvoke.cs
+++ SDL3-CS/SDL/Audio/audio/PInvoke.cs
@@ -1244,7 +1244,7 @@ public static partial class SDL
     /// <param name="stream">the audio stream associated with the audio device to query.</param>
     /// <returns><c>true</c> if device is valid and paused, <c>false</c> otherwise.</returns>
     /// <threadsafety>It is safe to call this function from any thread.</threadsafety>
-    /// <since>This function is available since SDL 3.2.0.</since>
+    /// <since>This function is available since SDL 3.1.3.</since>
     /// <seealso cref="PauseAudioStreamDevice"/>
     /// <seealso cref="ResumeAudioStreamDevice"/>
     [LibraryImport(SDLLibrary, EntryPoint = "SDL_AudioStreamDevicePaused"), UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
