3574b25d6c9c39d20d56f50cf37ba47e408812c4
Eduard Gushchin <eduardgushchin@yandex.ru>
2025-02-05 15:02:36 +0300

    Fix warnings

--- SDL3-CS/SDL/Input Events/mouse/PInvoke.cs
+++ SDL3-CS/SDL/Input Events/mouse/PInvoke.cs
@@ -244,7 +244,7 @@ public static partial class SDL
     /// report continuous relative mouse motion even if the mouse is at the edge of
     /// the window.</para>
     /// <para>If you'd like to keep the mouse position fixed while in relative mode you
-    /// can use <see cref="SetWindowMouseRect"/>. If you'd like the cursor to be at a
+    /// can use <see cref="SetWindowMouseRect(IntPtr, in Rect)"/>. If you'd like the cursor to be at a
     /// specific location when relative mode ends, you should use
     /// <see cref="WarpMouseInWindow"/> before disabling relative mode.</para>
     /// <para>This function will flush any pending mouse motion for this window.</para>
